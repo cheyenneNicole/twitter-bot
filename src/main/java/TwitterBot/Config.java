625346cc986f794f39852a57cb9436706231ee6b
Cheyenne <cheyenneholland@ymail.com>
2018-10-15 13:31:45 -0500

    Connected to dev account and updated status to test connection
    Added credentials to interact with the Twitter service,
    Added code to update the status of the twitter account to verify a connection.

--- src/main/java/TwitterBot/Config.java
+++ src/main/java/TwitterBot/Config.java
@@ -1,4 +1,13 @@
 package TwitterBot;
 
 public class Config {
+
+    protected final static String CONSUMER_KEY = "aOwXwg1NgGRhDiO4tsgvOKM6J";
+
+    protected final static String CONSUMER_SECRET = "x5hNENOZIDsl3zl6naPOkzbN1OWkTgSAXjTMVATUGaKdhDBXS5";
+
+    protected final static String ACCESS_TOKEN = "1043191496705490944-NIYgIVicEQpb90Fjt569iHsccVBTJG";
+
+    protected final static String ACCESS_TOKEN_SECRET = "6IO6Su0sQ0m3bJuq3xZbbpkdM4gBI9BEpMIJ4hPbkGd1c";
+
 }
--- src/main/java/TwitterBot/Main.java
+++ src/main/java/TwitterBot/Main.java
@@ -1,12 +1,31 @@
 package TwitterBot;
 
+import twitter4j.*;
+import twitter4j.auth.AccessToken;
+
+
 public class Main {
 
     public static void main(String[] args){
 
-        System.out.println("Hello world!!");
-
-
+        Twitter twitter = new TwitterFactory().getInstance();
+        // Twitter Consumer key & Consumer Secret
+        twitter.setOAuthConsumer(Config.CONSUMER_KEY, Config.CONSUMER_SECRET);
+        // Twitter Access token & Access token Secret
+        twitter.setOAuthAccessToken(new AccessToken(Config.ACCESS_TOKEN, Config.ACCESS_TOKEN_SECRET));
+        try {
+            // Getting Twitter Timeline using Twitter4j API
+            ResponseList statusReponseList = twitter.getUserTimeline(new Paging(1, 5));
+            for (int x = 0; x < statusReponseList.size(); x++) {
+                System.out.println(statusReponseList.get(x).toString());
+            }
+            // Post a Tweet using Twitter4j API
+            Status status = twitter.updateStatus("Hello world!");
+            System.out.println("Successfully updated the status to [" + status.getText() + "].");
+        } catch (Exception e) {
+        }
     }
-
 }
+
+
+
